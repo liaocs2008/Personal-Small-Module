e874cb6468ee72553455b6278ea7da19c4b3efcb
Leo <liaocs2008@users.noreply.github.com>
2014-12-16 18:38:16 -0500

    add a overwrite bug

--- leo-vim.sh
+++ leo-vim.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.vim
                                                                                    
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim        
                                                                                    
-# -q is to overwrite, -P is to specify output directory                            
-wget -q -P ~ https://raw.githubusercontent.com/liaocs2008/Personal-Small-Module/master/.vimrc
+# -N is to overwrite, -P is to specify output directory                            
+wget -N -P ~ https://raw.githubusercontent.com/liaocs2008/Personal-Small-Module/master/.vimrc
                                                                                    
 # -c is to run command after loading vim                                           
 vim -c :PluginInstall
